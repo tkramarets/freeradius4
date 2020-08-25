4fc90d399bff3a6b97f50997d098eb30106efdf4
Taras Kramarets <tkramarets@gmail.com>
2020-08-26 02:14:26 +0300

    a lot of fixes

--- Dockerfile
+++ Dockerfile
@@ -29,61 +29,54 @@ COPY --from=build /usr/local/src/repositories/*.deb /tmp/
 #
 
 # Database host
-ENV DB_HOST=localhost
+#ENV DB_HOST=localhost
 
 # Database port
-ENV DB_PORT=3306
+#ENV DB_PORT=3306
 
 # Database user
-ENV DB_USER=radius
+#ENV DB_USER=radius
 
 # Database password
-ENV DB_PASS=radpass
+#ENV DB_PASS=radpass
 
 # Database name
-ENV DB_NAME=radius
+#ENV DB_NAME=radius
 
 
 # EMAIL SMTP SETTINGS
 
 # smtp server
-ENV SMTP_SERVER=gmail.com
+#ENV SMTP_SERVER=gmail.com
 
 # smtp server port
-ENV SMTP_PORT=465
+#ENV SMTP_PORT=465
 
 # smtp username
-ENV SMTP_USER=admin@gmail.com
+#ENV SMTP_USER=admin@gmail.com
 
 # smtp user password
-ENV SMTP_PASS=superstrong_password
+#ENV SMTP_PASS=superstrong_password
 
 # module will send errors to this mailbox
-ENV SMTP_ADMIN_EMAIL=who_will_receive_email@example.com
+#ENV SMTP_ADMIN_EMAIL=who_will_receive_email@example.com
 
 # can be different than SMTP_USER like Envelope-from
-ENV SMTP_SENDER_EMAIL=admin@gmail.com
+#ENV SMTP_SENDER_EMAIL=admin@gmail.com
 
 # subject of this mail can be useful for filters etc.
-ENV SMTP_SUBJECT="Radius: Failed login attempt"
+#ENV SMTP_SUBJECT="Radius: Failed login attempt"
 
 
 ### Expired cache will be deleted in 
-### 30 days =  2592000 seconds
-ENV CACHE_TIME=2592000
+### 30 minutes 
+#ENV CACHE_TIME=30
 
 ### timeout to response
-ENV IMAP_TIMEOUT=5s
+#ENV IMAP_TIMEOUT=5s
 
 ### Imap module it's curl in background so here can be smtp://localhost:25
-ENV IMAP_URI=imap://localhost:993
-
-###
-#ENV IMAP_PROTO=tls
-#ENV IMAP_CERT=
-#ENV IMAP_CA=
-#ENV IMAP_KEY=
-#ENV IMAP_KEY_PASS=
+#ENV IMAP_URI=imap://localhost:993
 
 ENV FR_LIBRARY_PATH=/usr/lib/freeradius
 ENV LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libperl.so.5.26
@@ -94,13 +87,11 @@ RUN apt-get update \
     && ln -s /usr/sbin/freeradius /usr/sbin/radiusd
 
 RUN rm /etc/freeradius/mods-enabled/soh
-#COPY raddb /etc/freeradius
-COPY default /etc/freeradius/sites-enabled/default
 COPY perl  /etc/freeradius/mods-enabled/perl
 COPY rlm_perl.pl /etc/freeradius/mods-config/perl/rlm_perl.pl
 COPY clients.conf /etc/freeradius/clients.conf
 COPY imap /etc/freeradius/mods-enabled/imap
-
+RUN apt-get install -y libmysqlclient-dev libdbd-mysql-perl
 COPY docker-entrypoint.sh /
 VOLUME ["/etc/freeradius"]
 EXPOSE 1812 1813
@@ -111,3 +102,6 @@ RUN apt-get install -y cpanminus mc
 RUN cpanm  Email::Sender \
     Email::Simple \
     Email::Sender::Transport::SMTPS \
+    Email::Sender::Transport::SMTP::TLS \
+    Net::SMTP::TLS \
+    Try::Tiny \
--- rlm_perl.pl
+++ rlm_perl.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w 
 #####################################################################################################################
 #
 #  *Script purpose*
@@ -8,7 +8,7 @@
 #  Cache logic if not present in DB we return noop and pass authentication to imap module after successful
 #  authentication we remember user and pass to DB via rlm_perl
 #
-#  Cache timeout 
+#  Cache timeout
 #  Send Email on Failed authentication
 #
 #  writen by Taras.Kramarets aka tarasnix (c) 2020 freelancehunt project
@@ -16,276 +16,263 @@
 #
 ######################################################################################################################
 
-use strict;
-use warnings;
 use DBI;
 use Email::Sender::Simple qw(sendmail);
-use Email::Sender::Transport::SMTPS();
-use Email::Simple ();
-use Email::Simple::Creator ();
+use Email::Sender::Transport::SMTPS;
+use Email::Simple::Creator;
+use Try::Tiny;
 
+use vars qw/%RAD_PERLCONF %RAD_REQUEST %RAD_REPLY %RAD_CHECK %RAD_REQUEST_PROXY %RAD_REQUEST_PROXY_REPLY/;
 
-# Bring the global hashes into the package scope
-#our (%RAD_REQUEST, %RAD_REPLY, %RAD_CONFIG, %RAD_STATE);
-
-# This is hash wich hold original request from radius
-my %RAD_REQUEST;
-# In this hash you add values that will be returned to NAS.
-my %RAD_REPLY;
-#This is for config items (was %RAD_CHECK in earlier versions)
 my %RAD_CONFIG;
-# This is the session-sate
-my %RAD_STATE;
-# This is configuration items from "config" perl module configuration section
-my %RAD_PERLCONF;
-
-my %RAD_CHECK;
 
 use constant {
-	RLM_MODULE_REJECT   => 0, # immediately reject the request
-	RLM_MODULE_OK       => 2, # the module is OK, continue
-	RLM_MODULE_HANDLED  => 3, # the module handled the request, so stop
-	RLM_MODULE_INVALID  => 4, # the module considers the request invalid
-	RLM_MODULE_DISALLOW => 5, # reject the request (user is locked out)
-	RLM_MODULE_NOTFOUND => 6, # user not found
-	RLM_MODULE_NOOP     => 7, # module succeeded without doing anything
-	RLM_MODULE_UPDATED  => 8, # OK (pairs modified)
-	RLM_MODULE_NUMCODES => 9  # How many return codes there are
+        RLM_MODULE_REJECT   => 0, # immediately reject the request
+        RLM_MODULE_OK       => 2, # the module is OK, continue
+        RLM_MODULE_HANDLED  => 3, # the module handled the request, so stop
+        RLM_MODULE_INVALID  => 4, # the module considers the request invalid
+        RLM_MODULE_DISALLOW => 5, # reject the request (user is locked out)
+        RLM_MODULE_NOTFOUND => 6, # user not found
+        RLM_MODULE_NOOP     => 7, # module succeeded without doing anything
+        RLM_MODULE_UPDATED  => 8, # OK (pairs modified)
+        RLM_MODULE_NUMCODES => 9  # How many return codes there are
 };
 
-use constant {
-	L_AUTH         => 2,  # Authentication message
-	L_INFO         => 3,  # Informational message
-	L_ERR          => 4,  # Error message
-	L_WARN         => 5,  # Warning
-	L_PROXY        => 6,  # Proxy messages
-	L_ACCT         => 7,  # Accounting messages
-	L_DBG          => 16, # Only displayed when debugging is enabled
-	L_DBG_WARN     => 17, # Warning only displayed when debugging is enabled
-	L_DBG_ERR      => 18, # Error only displayed when debugging is enabled
-	L_DBG_WARN_REQ => 19, # Less severe warning only displayed when debugging is enabled
-	L_DBG_ERR_REQ  => 20, # Less severe error only displayed when debugging is enabled
-};
 
+
+use constant    L_DBG=>         1;
+use constant    L_AUTH=>        2;
+use constant    L_INFO=>        3;
+use constant    L_ERR=>         4;
+use constant    L_PROXY=>       5;
+use constant    L_CONS=>        128;
+
+
+&radiusd::log(L_DBG, '-=======================perl module======================-');
+
+
+&cache_expiration();
 
 sub failed_auth {
+&cache_expiration();
 # email credentials
-	my $smtpserver = $RAD_PERLCONF{'smtp'}->{'server'};
-	my $smtpport = $RAD_PERLCONF{'smtp'}->{'port'};
-	my $smtpuser   = $RAD_PERLCONF{'smtp'}->{'user'};
-	my $smtppassword = $RAD_PERLCONF{'smtp'}->{'pass'};
+&radiusd::log(L_DBG, '-=======================failed auth global===========================');
 
-my $transport = Email::Sender::Transport::SMTPS->new({
-	host => $smtpserver,
-	port => $smtpport,
-	ssl  => 'starttls',
-	sasl_username => $smtpuser,
-	sasl_password => $smtppassword,
-});
+       my $smtpserver = $RAD_PERLCONF{'smtp'}->{'server'};
+       my $smtpport = $RAD_PERLCONF{'smtp'}->{'port'};
+       my $smtpuser   = $RAD_PERLCONF{'smtp'}->{'user'};
+       my $smtppassword = $RAD_PERLCONF{'smtp'}->{'pass'};
 
+&radiusd::log(L_DBG, '-=====================FAILED AUTH=======================-');
+
+my $transport = Email::Sender::Transport::SMTPS->new(
+        host => "$smtpserver",
+        port => $smtpport,
+        ssl  => 1,
+        sasl_username => "$smtpuser",
+        sasl_password => "$smtppassword",
+        debug => 1,
+        timeout => 5,
+);
+        my $radiusUser = $RAD_REQUEST{'User-Name'};
+        my $radiusNas = $RAD_REQUEST{'NAS-IP-Address'};
+        my $radiusFail = $RAD_REQUEST{'Module-Failure-Message'};
+        my $radiusNasi = $RAD_REQUEST{'NAS-Identifier'};
 my $email = Email::Simple->create(
-	header => [
-	To      => $RAD_PERLCONF{'smtp'}->{'admin_email'},
-	From    => $RAD_PERLCONF{'smtp'}->{'sender_email'},
-	Subject => $RAD_PERLCONF{'smtp'}->{'subject'},
+        header => [
+        To      => 'tkramarets@gmail.com',
+        From    => 'myradius@i.ua',
+        Subject => "hello from radius server",
 ],
-	body => "Failed authentication from $RAD_REQUEST{'User-Name'} \n
-	
-	",
+        body => "Failed authentication from $radiusUser on $radiusNas ( $radiusNasi ) $radiusFail",
 );
 
+try {
+
+&radiusd::log(L_DBG, '-=====================FAILED AUTH Send email to admin=======================-'.$_);
 sendmail($email, { transport => $transport });
+
+}
+catch {
+die "Error Email Sending: $_";
+&radiusd::log(L_DBG, '-=====================FAILED AUTH=======================-'.$_);
+
+};
 }
 
 
 
 # Function to handle authorize
 sub authorize {
-	# For debugging purposes only
-	log_request_attributes();
+&cache_expiration();
 
-	# Here's where your authorization code comes
-	# You can call another function from here:
-	test_call();
-
-	return RLM_MODULE_OK;
+&radiusd::log(L_DBG, '-=======================Authorize=======================-');
+        log_request_attributes();
+	&check_in_cache();
 }
 
 
 sub authenticate {
-	# For debugging purposes only
-	log_request_attributes();
-
+&cache_expiration();
+&radiusd::log(L_DBG, '-=======================Authenticate====================-');
+        log_request_attributes();
 	&check_in_cache();
-
 }
 
 
 # Function to handle preacct
 sub preacct {
-	# For debugging purposes only
-	log_request_attributes();
-
-	return RLM_MODULE_OK;
+&cache_expiration();
+&radiusd::log(L_DBG, '-=======================Preacct====================-');
+        log_request_attributes();
 }
 
 
 # Function to handle accounting
 sub accounting {
-	# For debugging purposes only
-	log_request_attributes();
-
-	# You can call another subroutine from here
-	&test_call();
-
-	return RLM_MODULE_OK;
+&cache_expiration();
+&radiusd::log(L_DBG, '-=======================Post_auth====================-');
+        log_request_attributes();
 }
 
 
 # Function to handle pre_proxy
 sub pre_proxy {
-	# For debugging purposes only
-	log_request_attributes();
-
-	return RLM_MODULE_OK;
+&cache_expiration();
+&radiusd::log(L_DBG, '-=======================Pre_proxy====================-');
+        log_request_attributes();
 }
 
 # Function to handle post_proxy
 sub post_proxy {
-	# For debugging purposes only
-	log_request_attributes();
+&radiusd::log(L_DBG, '-=======================Post_proxy====================-');
+        log_request_attributes();
 
-	return RLM_MODULE_OK;
 }
 
 # Function to handle post_auth
 sub post_auth {
-	log_request_attributes();
-		&failed_auth();
-	return RLM_MODULE_OK;
+&cache_expiration();
+&radiusd::log(L_DBG, '-=======================Post_auth====================-');
+log_request_attributes();
+my $msgs=$RAD_REQUEST{'Module-Failure-Message'};
+#= $RAD_REQUEST{''};
+if ( $msgs ne ''  ) {
+&radiusd::log(L_INFO, "POST AUTH FAIL");
+&failed_auth();
+return RLM_MODULE_REJECT;
+}
+else {
+&radiusd::log(L_INFO, "POST AUTH OK insert to cache");
+&insert_to_cache();
+return RLM_MODULE_OK;
+}
 }
 
-
-# Function to handle xlat
-sub xlat {
-	# For debugging purposes only
-#	log_request_attributes();
-
-	# Loads some external perl and evaluate it
-	my ($filename,$a,$b,$c,$d) = @_;
-	radiusd::radlog(L_DBG, "From xlat $filename");
-	radiusd::radlog(L_DBG,"From xlat $a $b $c $d");
-	local *FH;
-	open FH, $filename or die "open '$filename' $!";
-	local($/) = undef;
-	my $sub = <FH>;
-	close FH;
-	my $eval = qq{ sub handler{ $sub;} };
-	eval $eval;
-	eval {main->handler;};
-}
 
 # Function to handle detach
 sub detach {
-	# For debugging purposes only
-#	log_request_attributes();
+&cache_expiration();
+&radiusd::log(L_DBG, '-=======================Detach====================-');
+       log_request_attributes();
 }
 
 
 
 sub dbConnect {
-	my $dbHost		= $RAD_PERLCONF{'db'}->{'host'};
-	my $dbName 		= $RAD_PERLCONF{'db'}->{'name'};
-	my $dbUsername	= $RAD_PERLCONF{'db'}->{'user'};
-	my $dbPassword 	= $RAD_PERLCONF{'db'}->{'password'};
+&radiusd::log(L_DBG, '-=======================Connect to the Database====================-');
 
-my $dbh = DBI->connect("DBI:mysql:dbname=$dbName;host=$dbHost", $dbUsername, $dbPassword) or &radiusd::radlog(L_ERR, "DB connection failed: " . DBI->errstr);
+        my $dbHost              = $RAD_PERLCONF{'db'}->{'host'};
+        my $dbName              = $RAD_PERLCONF{'db'}->{'name'};
+        my $dbUsername  = $RAD_PERLCONF{'db'}->{'user'};
+        my $dbPassword  = $RAD_PERLCONF{'db'}->{'password'};
+
+my $dbh = DBI->connect("DBI:mysql:database=$dbName;host=$dbHost", $dbUsername, $dbPassword);
+ return $dbh;
 };
 
-sub check_in_cache { 
+sub check_in_cache {
+&radiusd::log(L_DBG, '-=======================Check that user is present in cache ====================-');
 
-	&dbConnect();
-	
-	my $radiusUserPassword = $RAD_REQUEST{'User-Password'};
-        my $username = $RAD_REQUEST{'Username'};
-	unless (my $dbh) {
-	
-	my $query = $dbh->prepare("SELECT * FROM cache_users WHERE username=? LIMIT 1");
-	
-	$query->execute($username);
+        $dbh=&dbConnect();
 
-	my $result = $query->fetchrow_arrayref();
+        my $radiusUserPassword = $RAD_REQUEST{'User-Password'};
+        my $username = $RAD_REQUEST{'User-Name'};
+
+        my $query = $dbh->prepare("SELECT * FROM cached_users WHERE username=? LIMIT 1");
+
+        $query->execute($username);
+
+        my $result = $query->fetchrow_arrayref();
+
+        my $password = @$result[1];
 	
-	my $password = @$result[2];
-	
-	$query->finish();
-	
-	$dbh->disconnect();
-	
-	
-	if ($password == $radiusUserPassword) { 
-	
-		return RLM_MODULE_OK;
-	
-	}
-	
-	else {
-	
-		return RLM_MODULE_NOOP;
-	
-	}
-}
+        $query->finish();
+
+        $dbh->disconnect();
+
+
+        if ($password eq $radiusUserPassword) {
+        &radiusd::log(L_DBG, '-=======================User is present in cache Fininsh====================-');
+		$RAD_REPLY{'Auth-Type'}='Perl';
+                return RLM_MODULE_OK;
+
+        }
+        else {
+
+        &radiusd::log(L_DBG, "-=======================User isn't present in cache ====================-");
+              return RLM_MODULE_NOOP;
+
+        }
 }
 
 sub cache_expiration {
-	log_request_attributes();
-	
-	&dbConnect();
-        my $username = $RAD_REQUEST{'Username'};
-	
-	unless (my $dbh) {
-	
-	my $query = $dbh->prepare("SELECT * FROM cache_users WHERE cache_time  LIMIT 1");
-	
-	$query->execute($username);
-	
-	my $result = $query->fetchrow_arrayref();
-	
-	my $password = @$result[2];
-	
-	$query->finish();
-	
-	$dbh->disconnect();
-	
-	return RLM_MODULE_NOOP;
+        &radiusd::log(L_DBG, '-=======================Check cache expiration====================-');
+
+        log_request_attributes();
+
+        $dbh=&dbConnect();
+
+        my $query = $dbh->prepare("delete from cached_users where timestamp < (NOW() - INTERVAL $ENV{'CACHE_TIME'} MINUTE)");
+
+        $query->execute();
+
+        $query->finish();
+
+        $dbh->disconnect();
+
+        return RLM_MODULE_NOOP;
 }
-}
+
 
 sub insert_to_cache {
-	log_request_attributes();
-	
-	&dbConnect();
-	unless(my $dbh) {
-	$dbh->do("INSERT INTO cache_users VALUES (?, ?)", $RAD_REQUEST{'User-Name'}, $RAD_REQUEST{'Cleartext-Password'});
-	$dbh->disconnect();
-	}
-	return RLM_MODULE_OK;
+        &radiusd::log(L_DBG, '-=======================Insert user to the cache====================-');
+
+	$username=$RAD_REQUEST{'User-Name'};
+	$password=$RAD_REQUEST{'User-Password'};
+        log_request_attributes();
+        $dbh=&dbConnect();
+        my $query=$dbh->prepare("INSERT INTO cached_users(username,password) VALUES (?,?)");
+        $query->execute($username,$password);
+        $query->finish();
+        $dbh->disconnect();
+        return RLM_MODULE_OK;
 }
 
-
-sub test_call {
-	# Some code goes here
-}
 
 sub log_request_attributes {
-	# log all request data for debug
-	for (keys %RAD_REQUEST) {
-	&radiusd::radlog(L_DBG, "RAD_REQUEST: $_ = $RAD_REQUEST{$_}");
-	}
-	for (keys %RAD_CHECK) {
-	&radiusd::radlog(L_DBG, "RAD_CHECK: $_ = $RAD_CHECK{$_}");
-	}
-	for (keys %RAD_PERLCONF) {
-	&radiusd::radlog(L_DBG, "RAD_PERLCONF: $_ = $RAD_PERLCONF{$_}");
-	}
+        # log all request data for debug
+        for (keys %RAD_REQUEST) {
+        &radiusd::log(L_DBG, "RAD_REQUEST: $_ = $RAD_REQUEST{$_}");
+        }
+        for (keys %RAD_CHECK) {
+        &radiusd::log(L_DBG, "RAD_CHECK: $_ = $RAD_CHECK{$_}");
+        }
+        for (keys %RAD_PERLCONF) {
+        &radiusd::log(L_DBG, "RAD_PERLCONF: $_ = $RAD_PERLCONF{$_}");
+        }
+        for (keys %RAD_CONFIG) {
+        &radiusd::log(L_DBG, "RAD_CONFIG: $_ = $RAD_CONFIG{$_}");
+        }
 }
+
