79a62b21049fe6c3b12a0bd0b05b5c2942e0abd1
Taras Kramarets <tkramarets@gmail.com>
2020-08-26 03:16:43 +0300

    add debug arg to Docker image

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,21 +6,36 @@ set -e
 # -v; -SL; -f arg; etc will work, but not arg1 arg2
 if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
     chown freerad:freerad -R /etc/freeradius
-    set -- freeradius -X "$@"
+    if [ -z "${RAD_DEBUG}" ]
+    then
+    set -- freeradius "$@"
+    else
+    set -- freeradius ${RAD_DEBUG_LEVEL} "$@"
+fi
 fi
 
 # check for the expected command
 if [ "$1" = 'freeradius' ]; then
     shift
     chown freerad:freerad -R /etc/freeradius
-    exec freeradius -f -X "$@"
+    if [ -z "${RAD_DEBUG}" ]
+    then
+    set -- freeradius -f "$@"
+    else
+    set -- freeradius -f ${RAD_DEBUG_LEVEL} "$@"
+fi
 fi
 
 # many people are likely to call "radiusd" as well, so allow that
 if [ "$1" = 'radiusd' ]; then
     shift
     chown freerad:freerad -R /etc/freeradius
-    exec freeradius -f -X "$@"
+    if [ -z "${RAD_DEBUG}" ]
+    then
+    set -- freeradius -f "$@"
+    else
+    set -- freeradius -f ${RAD_DEBUG_LEVEL} "$@"
+fi
 fi
 
 # else default to run whatever the user wanted like "bash" or "sh"
